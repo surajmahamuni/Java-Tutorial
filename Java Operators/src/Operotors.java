a84f52876c401c13dcb0eac8473ffb3fc14e3466
Suraj Mahamuni <suraj.r.mahamuni.26@gmail.com>
2019-02-25 18:15:35 -0800

    Java Operators

---
+++ Java Operators/src/Operotors.java
@@ -0,0 +1,29 @@
+
+public class Operotors {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		int result = 0;
+		
+		result = a + b;  // addition operator
+		result = a - b;  //subtraction operator
+		result = a * b;  //multiplication operator
+		result = a / b;  //Division Operator
+		result = a % b;  //remainder Operator
+		
+		result += result;  // result = result + result
+		result -= result;  // result = result - result
+		result *= result;  // result = result * result
+		result /= result;  // result = result / result
+		
+		// a > b greater than operator
+		// a < b less than operator
+		// a >= b greater than or equal to
+		// a <= b less than or equal to
+		// a == b to check both are equal or not
+		
+	}
+
+}
