d67211dd25ce957a9ab5f7ecb885a0960fb05c0e
Suraj Mahamuni <suraj.r.mahamuni.26@gmail.com>
2019-02-25 18:48:22 -0800

    Additional Operators

--- Java Operators/src/Operotors.java
+++ Java Operators/src/Operotors.java
@@ -7,23 +7,42 @@ public class Operotors {
 		int b = 20;
 		int result = 0;
 		
+		
+		// Arithmetic Operators 
 		result = a + b;  // addition operator
 		result = a - b;  //subtraction operator
 		result = a * b;  //multiplication operator
 		result = a / b;  //Division Operator
 		result = a % b;  //remainder Operator
 		
+		
+		// Unary Operator
+		
+		result++;  // increament operator
+		result--;  // decreament Operator
+		// ! logical complement operator, inverts value of boolean 
+		
+		
+		
 		result += result;  // result = result + result
 		result -= result;  // result = result - result
 		result *= result;  // result = result * result
 		result /= result;  // result = result / result
 		
+		
+		// **********Equality and relational operator************
 		// a > b greater than operator
 		// a < b less than operator
 		// a >= b greater than or equal to
 		// a <= b less than or equal to
 		// a == b to check both are equal or not
+		// a != b not equal to 
 		
+		
+		// Conditional Operator
+		// && conditional AND operator
+		// || conditional OR operator
+		// ?: ternary operator  eg. if then else statement  , if condition before : is true returns true if not then boolean value after :
 	}
 
 }
