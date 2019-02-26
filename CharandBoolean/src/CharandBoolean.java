001914721299b25135fd4bab72151575988462c2
Suraj Mahamuni <suraj.r.mahamuni.26@gmail.com>
2019-02-25 17:28:58 -0800

    Character and Boolean

---
+++ CharandBoolean/src/CharandBoolean.java
@@ -0,0 +1,15 @@
+
+public class CharandBoolean {
+
+	public static void main(String[] args) {
+		
+		// width of 2 bytes
+				char myChar = 'A'; // stores single character from a-z , A-Z, 0-9, and unicode characters
+				System.out.println("My character is : " +myChar);
+				boolean isMale = true; // boolean can have value of true or false
+				
+				
+
+	}
+
+}
